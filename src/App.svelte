0ceab064cf39f045a6bede1df9e4173a249c65de
justin.shilling <loveofmusic25@hotmail.com>
2021-05-26 10:10:04 -0400

    removed unused template boilerplate

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,31 @@
 <script>
-	export let name;
+	import { onMount } from "svelte";
+	import axios from "axios";
+
+
+    // functions
+	function getIP() {
+        console.log("getting IP...");
+		// axios.get("https://geo.ipify.org/api/v1?apiKey=at_7BluHqXMgTh0XFpv0H4xunOsHVpCg")
+        //     .then((response) => {
+        //         const { data } = response;
+        //         console.log(data);
+        //     }).catch(error => {
+        //         console.log(error);
+        //     });
+	}
+
+    // mount
+	onMount(async () => {
+        getIP();
+    });
+
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<header>
+        <h1>IP Address Tracker</h1>
+    </header>
 </main>
 
 <style>
@@ -13,11 +34,12 @@
 		padding: 1em;
 		max-width: 240px;
 		margin: 0 auto;
+        background-image: url("/images/pattern-bg.png");
+        background-size: cover;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color: #fff;
 		font-size: 4em;
 		font-weight: 100;
 	}
