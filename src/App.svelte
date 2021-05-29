16a7eed19458aa30cf1c1fdb58988b3c298fd843
Justin Shilling <loveofmusic25@hotmail.com>
2021-05-29 11:00:16 -0400

    integrating mapbox

--- src/App.svelte
+++ src/App.svelte
@@ -1,38 +1,88 @@
 <script>
 	import { onMount } from "svelte";
+	import { fly } from "svelte/transition";
 	import axios from "axios";
 	import FontIcon from "svelte-fa";
 	import { faChevronRight } from "@fortawesome/free-solid-svg-icons";
+	import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
+
+	// constants
+	const fadeUpIn = { y: 30, duration: 500 };
 
 	// state
-	let ipLat = 0;
-	let ipLng = 0;
+	let map;
+	let locationMarker;
+	let ipData = {
+		ip: "",
+		isp: "",
+		name: "",
+		location: {
+			city: "",
+			country: "",
+			lat: 39.8,
+			lng: -89.5,
+			postalCode: "",
+			region: "",
+			timezone: "",
+		},
+	};
+	let searchInput = null;
+    let dataIsLoading = false;
 
 	// functions
-	function getIP() {
-		console.log("getting IP...");
-		// axios
-		// 	.get("https://geo.ipify.org/api/v1?apiKey=at_7BluHqXMgTh0XFpv0H4xunOsHVpCg")
-		// 	.then((response) => {
-		// 		const { data } = response;
-		// 		console.log(data);
-		// 		ipLat = data.location.lat;
-		// 		ipLng = data.location.lng;
-		// 		console.log(ipLat);
-		// 		console.log(ipLng);
-		// 	})
-		// 	.catch((error) => {
-		// 		console.log(error);
-		// 	});
+	async function getIP() {
+		// console.log("getting IP...");
+        enterLoading();
+		let domain = searchInput == null ? "" : `&domain=${searchInput}`;
+		try {
+			const response = await axios.get(`https://geo.ipify.org/api/v1?apiKey=${JSON.parse(__myapp.env.API_KEY_IPIFY)}${domain}`);
+			const { data } = response;
+			ipData = { ...data };
+            dataIsLoading = false;
+			updateMapView();
+            searchInput = null;
+			return response;
+		} catch (error) {
+			console.log(error);
+            searchInput = null;
+		}
+	}
 
-		console.log(ipLat);
-		console.log(ipLng);
-        ipLat = 40;
-        ipLng = 30;
+	function updateMapView() {
+		// console.log("updating map view...");
+		const { lng, lat } = ipData.location;
+        map.flyTo({center: [lng, lat], zoom: 7.5});
+		locationMarker.setLngLat([lng, lat]);
+	}
+
+	function initMap() {
+		// console.log("initializing map...");
+		const { lng, lat } = ipData.location;
+		mapboxgl.accessToken = JSON.parse(__myapp.env.API_KEY_MAPBOX);
+		map = new mapboxgl.Map({
+			container: "map",
+			style: "mapbox://styles/mapbox/streets-v11",
+			center: [lng, lat],
+			zoom: 3,
+		});
+		locationMarker = new mapboxgl.Marker().setLngLat([lng, lat]).addTo(map);
+	}
+
+    function enterLoading() {
+        const { lng, lat } = ipData.location;
+        dataIsLoading = true;
+        map.flyTo({center: [lng, lat], zoom: 3});
+    }
+
+	function onKeyPress(e) {
+		if (e.charCode === 13) {
+			getIP();
+		}
 	}
 
 	// mount
 	onMount(async () => {
+		initMap();
 		getIP();
 	});
 </script>
@@ -42,17 +92,55 @@
 		<h1>IP Address Tracker</h1>
 
 		<div class="search-wrapper">
-			<input type="text" placeholder="Search for any IP address or domain" />
+			<input type="text" placeholder="Search for IP address by domain" bind:value={searchInput} on:keypress={onKeyPress} />
 			<button on:click={getIP}>
 				<FontIcon icon={faChevronRight} color="#fff" size=".8x" class="searchIcon" />
 			</button>
 		</div>
 
 		<div class="results-container">
-			<div class="resultBox ipAddress" />
-			<div class="resultBox location" />
-			<div class="resultBox timezone" />
-			<div class="resultBox isp" />
+			<div class="resultBox ipAddress">
+				<h2 class="heading">IP Address</h2>
+				{#if ipData.ip != "" && !dataIsLoading}
+					<p class="value" transition:fly={fadeUpIn}>
+						{ipData.ip}
+					</p>
+                {:else}
+                    <p class="value">--</p>
+				{/if}
+			</div>
+			<div class="resultBox location">
+				<h2 class="heading">Location</h2>
+				{#if ipData.location.city != "" && !dataIsLoading}
+					<p class="value" transition:fly={fadeUpIn}>
+						{ipData.location.city}{#if ipData.location.city !== ""},{/if}
+						{ipData.location.region}
+					</p>
+                {:else}
+                    <p class="value">--</p>
+				{/if}
+			</div>
+			<div class="resultBox timezone">
+				<h2 class="heading">Timezone</h2>
+				{#if ipData.location.timezone != "" && !dataIsLoading}
+					<p class="value" transition:fly={fadeUpIn}>
+						{#if ipData.location.timezone !== ""}UTC{/if}
+						{ipData.location.timezone}
+					</p>
+                {:else}
+                    <p class="value">--</p>
+				{/if}
+			</div>
+			<div class="resultBox isp">
+				<h2 class="heading">ISP</h2>
+				{#if ipData.isp != "" && !dataIsLoading}
+					<p class="value" transition:fly={fadeUpIn}>
+						{ipData.isp}
+					</p>
+                {:else}
+                    <p class="value">--</p>
+				{/if}
+			</div>
 		</div>
 	</header>
 
@@ -91,6 +179,33 @@
 				:global(.searchIcon) {
 					vertical-align: 0 !important;
 				}
+			}
+		}
+	}
+
+	.results-container {
+		position: absolute;
+		top: 20rem;
+		left: 50%;
+		width: 80%;
+		transform: translateX(-50%);
+		z-index: 100;
+		display: flex;
+		padding: 4rem;
+		background-color: #fff;
+		border-radius: 10px;
+		box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.3);
+		.resultBox {
+			margin: 0 3rem;
+			width: 25%;
+			.heading {
+				margin-bottom: 1rem;
+				font-weight: 300;
+				color: rgb(141, 138, 138);
+			}
+			.value {
+				font-size: 2.6rem;
+				font-weight: 600;
 			}
 		}
 	}
